c2867bd393d20b012dfeb138ed42d8efd50aef12
Evgeni Golov <evgeni@golov.de>
2025-12-18 19:44:43 +0100

    control tx/idle state of the cc1101 from funkbus component

--- components/funkbus/light.py
+++ components/funkbus/light.py
@@ -1,7 +1,9 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.components import light
+from esphome.components import light, cc1101
 from esphome import pins
+
+DEPENDENCIES = ["cc1101"]
 
 funkbus_ns = cg.esphome_ns.namespace("funkbus")
 FunkbusLightOutput = funkbus_ns.class_("FunkbusLightOutput", cg.Component, light.LightOutput)
@@ -10,9 +12,11 @@ CONF_FUNKBUS_SERIAL = 'serial'
 CONF_FUNKBUS_SWITCH = 'switch'
 CONF_FUNKBUS_GROUP = 'group'
 CONF_FUNKBUS_PIN = 'pin'
+CONF_FUNKBUS_CC1101 = 'cc1101'
 
 CONFIG_SCHEMA = light.light_schema(FunkbusLightOutput, light.LightType.BINARY).extend({
   cv.Required(CONF_FUNKBUS_PIN): pins.internal_gpio_output_pin_schema,
+  cv.Required(CONF_FUNKBUS_CC1101): cv.use_id(cc1101.CC1101Component),
   cv.Required(CONF_FUNKBUS_SERIAL): cv.int_,
   cv.Optional(CONF_FUNKBUS_SWITCH, default=0): cv.int_,
   cv.Optional(CONF_FUNKBUS_GROUP, default=0): cv.int_,
@@ -25,6 +29,10 @@ async def to_code(config):
 
     pin = await cg.gpio_pin_expression(config[CONF_FUNKBUS_PIN])
     cg.add(var.set_pin(pin))
+
+    cc1101 = await cg.get_variable(config[CONF_FUNKBUS_CC1101])
+    cg.add(var.set_cc1101(cc1101))
+
     cg.add(var.set_serial(config[CONF_FUNKBUS_SERIAL]))
     cg.add(var.set_switch(config[CONF_FUNKBUS_SWITCH]))
     cg.add(var.set_group(config[CONF_FUNKBUS_GROUP]))
